c0c91cf4b9e93a067c6f6ab2ebc402751438c523
HanneSips <hanne.sips@publiq.be>
2023-02-20 22:35:26 +0100

    input js editors

--- src/components/input_selection/input_selection.js
+++ src/components/input_selection/input_selection.js
@@ -1,27 +1,58 @@
+import React, { useState } from "react";
+import { Controlled } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/javascript/javascript";
+
 function InputSelection() {
-  // function to configure a new input datastream and link it to a variable
-  var dataStreams = []
-  
+  const [dataStreams, setDataStreams] = useState([]);
+
   function configureNewInput() {
-    var dataStream = addInputDataStream()
-    linkStreamToVariable(dataStream)
+    setDataStreams((prevDataStreams) => [...prevDataStreams, { name: "", code: "" }]);
   }
 
-  // 
-  function addInputDataStream() {
-
+  function updateName(index, name) {
+    console.log("test name")
+    setDataStreams((prevDataStreams) => {
+      const newDataStreams = [...prevDataStreams];
+      newDataStreams[index].name = name;
+      return newDataStreams;
+    });
   }
 
-  //
-  function linkStreamToVariable(dataStream) {
-
+  function updateCode(index, code) {
+    console.log("test code")
+    setDataStreams((prevDataStreams) => {
+      const newDataStreams = [...prevDataStreams];
+      newDataStreams[index].code = code;
+      return newDataStreams;
+    });
   }
 
-  return <div>
-    <h3> input selection </h3>
-    <button onClick={configureNewInput}>+</button>
-    // LIST ALL INPUTS OF ARRAY dataStreams
-  </div>
+  return (
+    <div>
+      <h3>Input Selection</h3>
+      <button onClick={configureNewInput}>+</button>
+      {dataStreams.map((dataStream, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            value={dataStream.name}
+            onChange={(event) => updateName(index, event.target.value)}
+          />
+          <Controlled
+            value={dataStream.code}
+            options={{
+              mode: "javascript",
+              theme: "material",
+              lineNumbers: true,
+            }}
+            onBeforeChange={(editor, data, value) => {console.log("code updated") ; updateCode(index, value); }}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default InputSelection
+export default InputSelection;
